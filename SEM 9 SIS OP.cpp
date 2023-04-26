410cb6464be16e598ba656b07946ae8acac41743
NihilistSlayer <131922561+NihilistSlayer@users.noreply.github.com>
2023-04-26 20:05:36 -0300

    Add files via upload

--- SEM 9 SIS OP.cpp
+++ SEM 9 SIS OP.cpp
@@ -1,45 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <thread>
 #include <mutex>
+using namespace std;
 
-// Definindo o número de threads que vão escrever no arquivo
-#define NUM_THREADS 5
+mutex mtx;
 
-// Função que será executada pelas threads
-void threadFunction(int threadID, std::mutex& mutex, std::ofstream& outputFile)
-{
-    // Bloqueia o mutex para que apenas uma thread acesse o arquivo por vez
-    mutex.lock();
 
-    // Escreve no arquivo
-    outputFile << "Thread " << threadID << " escreveu no arquivo" << std::endl;
 
-    // Libera o mutex para que outras threads possam acessar o arquivo
-    mutex.unlock();
+void escrever_no_arquivo(int id) {
+    ofstream arquivo("arquivo.txt", ios::app);
+    if (arquivo.is_open()) {
+        mtx.lock();
+        
+        arquivo << "Thread " << (id+1) << " escreveu no arquivo" << endl;
+        std::cout << "Thread " << (id+1) << " escreveu no arquivo" << endl;
+        mtx.unlock();
+        arquivo.close();
+    }
 }
 
-int main()
-{
-    // Abre o arquivo para escrita
-    std::ofstream outputFile("arquivo.txt");
-
-    // Cria um mutex
-    std::mutex mutex;
-
-    // Cria as threads que vão escrever no arquivo
-    std::thread threads[NUM_THREADS];
-    for (int i = 0; i < NUM_THREADS; i++) {
-        threads[i] = std::thread(threadFunction, i, std::ref(mutex), std::ref(outputFile));
+int main() {
+    thread threads[5];
+    for (int i = 0; i < 5; i++) {
+        threads[i] = thread(escrever_no_arquivo, i);
     }
-
-    // Espera as threads terminarem de executar
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < 5; i++) {
         threads[i].join();
     }
-
-    // Fecha o arquivo
-    outputFile.close();
-
     return 0;
 }
